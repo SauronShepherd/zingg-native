c1b7144ea95b9b71610af87a71f4c886ae48d899
angel.alvarez <angel.alvarez.pascua@gmail.com>
2026-08-17 10:28:40 +0200

    fix: lazy-load optional pyspark prototypes

--- python/src/zingg_native/__init__.py
+++ python/src/zingg_native/__init__.py
@@ -11,7 +11,26 @@ if not hasattr(socketserver, "UnixStreamServer"):
 from .config import NativeConfig
 from .errors import BackendUnavailableError, UnsupportedOperationError, ZinggNativeError
 from .runtime import RuntimeInfo, detect_runtime
-from .similarity import exact, exact_similarity, jaccard_similarity, jaro_similarity
 from .zingg import Zingg
+
+
+def exact_similarity(*args, **kwargs):
+    from .similarity import exact_similarity as implementation
+    return implementation(*args, **kwargs)
+
+
+def exact(*args, **kwargs):
+    from .similarity import exact as implementation
+    return implementation(*args, **kwargs)
+
+
+def jaccard_similarity(*args, **kwargs):
+    from .similarity import jaccard_similarity as implementation
+    return implementation(*args, **kwargs)
+
+
+def jaro_similarity(*args, **kwargs):
+    from .similarity import jaro_similarity as implementation
+    return implementation(*args, **kwargs)
 
 __all__ = ["Zingg", "exact", "exact_similarity", "jaccard_similarity", "jaro_similarity", "RuntimeInfo", "detect_runtime", "NativeConfig", "ZinggNativeError", "BackendUnavailableError", "UnsupportedOperationError"]
